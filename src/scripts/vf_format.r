c91aae657870d4fdc603aa2f4b6175c680cfb2eb
Jonas C Kasmanas <54112176+JotaKas@users.noreply.github.com>
2023-09-25 21:42:37 +0200

    Generalize

--- src/scripts/vf_format.r
+++ src/scripts/vf_format.r
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-#Format Victors and VFDB output for mSpreadComp
+#Format Victors or VFDB output for gSpreadComp
 
 #### Load libs and inputs
 
@@ -9,62 +9,39 @@ library("dplyr")
 library("tidyr")
 
 option_list = list(
-  make_option(c("--victors"), type="character", default=NULL, 
-              help="Path to the created Victors merged result file", metavar="character"),
-  make_option(c("--vfdb"), type="character", default=NULL, 
-              help="Path to the created VFDB merged result file", metavar="character"),
-  make_option(c("--vic_header"), type="character", default=NULL, 
-              help="Path to the file with the headers from Victors database", metavar="character"),
-  make_option(c("--vfdb_header"), type="character", default=NULL, 
-              help="Path to the file with the headers from VFDB database", metavar="character"),			  
-  make_option(c("-o", "--out"), type="character",
-              help="output directory path to save formated tables", metavar="character")
-); 
+  make_option(c("--vf"), type="character", default=NULL, 
+              help="Path to the created blastx merged result file", metavar="character"),
+  make_option(c("--vf_header"), type="character", default=NULL, 
+              help="Path to the file with the headers from the selected vf database", metavar="character"),
+  make_option(c("--e_value"), type="numeric", default=1e-50, 
+              help="Evalue used in blastx", metavar="numeric"),
+  make_option(c("--vf_prefix"), type="character", default="both", 
+              help="Prefix to be used, based on the VF database name", metavar="character"),
+  make_option(c("-o", "--out"), type="character", 
+              help="Output directory path to save formatted tables", metavar="character")
+)
 
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
 out.path <- opt$out
 
-vic.path <- opt$victors
-vic.header.path <- opt$vic_header
-
-vfdb.path <- opt$vfdb
-vfdb.header.path <- opt$vfdb_header
-
+vf.path <- opt$vf
+vf.header.path <- opt$vf_header
+e_value <- opt$e_value
+vf_prefix <- opt$vf_prefix
 
 ##################################
 
 #1) Load BlastX results for diet bin mapped to VFDB2022 and VictorsDB
 
-vfdb_blastx_df <- data.table::fread(vfdb.path, sep = "\t")
-
-victors_blastx_df <- data.table::fread(vic.path, sep = "\t")
+vf_blastx_df <- data.table::fread(vf.path, sep = "\t")
 
 
-vfdb_headers <- data.table::fread(vfdb.header.path, header = F, sep = NULL)
-vfdb_headers$V1 <- sub(".", "", vfdb_headers$V1)
+vf_headers <- data.table::fread(vf.header.path, header = F, sep = NULL)
+vf_headers$V1 <- sub(".", "", vf_headers$V1)
 
-victors_headers <- data.table::fread(vic.header.path, header = F, sep = NULL)
-victors_headers$V1 <- sub(".", "", victors_headers$V1)
-
-names(vfdb_blastx_df) <- c('qseqid',
-                           'sseqid',
-                           'pident',
-                           'length',
-                           'mismatch',
-                           'gapopen',
-                           'qstart',
-                           'qend',
-                           'sstart',
-                           'send',
-                           'evalue',
-                           'bitscore',
-                           'qcovs',
-                           'qcovhsp',
-                           'mag')
-
-names(victors_blastx_df) <- c('qseqid',
+names(vf_blastx_df) <- c('qseqid',
                            'sseqid',
                            'pident',
                            'length',
@@ -82,99 +59,51 @@ names(victors_blastx_df) <- c('qseqid',
 
 
 #2) Filter out BlastX results 
-vfdb_blastx_df_filt <- vfdb_blastx_df %>%
-  filter(evalue < 1e-50)
 
-victors_blastx_df_filt <- victors_blastx_df %>%
-  filter(evalue < 1e-50)
+vf_blastx_df_filt <- vf_blastx_df %>%
+  filter(evalue < e_value)
 
 #3) Load Virulence Factors headers with annotation
-
-vfdb_headers_split <- tidyr::separate(vfdb_headers,
-                               V1,
-                               into = c("header_id","header_class"),sep = " ",
-                               remove = FALSE,extra = "merge") %>%
-  select(-V1) %>%
-  filter(!duplicated(header_id))
-
 ####
-victors_headers_split <- tidyr::separate(victors_headers,
+vf_headers_split <- tidyr::separate(vf_headers,
                                       V1,
                                       into = c("header_id","header_class"),sep = " ",
                                       remove = FALSE, extra = "merge") %>%
   select(-V1) %>%
   filter(!duplicated(header_id))
 
-#4) Merge Annotation to filtered dataset
+#4) Merge Annotation to the filtered dataset
 
-vfdb_merge <- merge.data.frame(x = vfdb_blastx_df_filt,
-                                  y = vfdb_headers_split,
-                                  by.x = "sseqid",
-                                  by.y = "header_id")
-
-vfdb_merge$mag <- gsub(pattern = ".out",
-                          replacement = "",
-                          x = vfdb_merge$mag)
-						  
-						  
-vfdb_merge$mag <- gsub(pattern = "vfdb_",
-                                   replacement = "",
-                                   x = vfdb_merge$mag)
-
-
-
-####
-
-victors_merge <- merge.data.frame(x = victors_blastx_df_filt,
-                 y = victors_headers_split,
+vf_merge <- merge.data.frame(x = vf_blastx_df_filt,
+                 y = vf_headers_split,
                  by.x = "sseqid",
                  by.y = "header_id")
 
-victors_merge$mag <- gsub(pattern = ".out",
+vf_merge$mag <- gsub(pattern = ".out",
                                    replacement = "",
-                                   x = victors_merge$mag)
+                                   x = vf_merge$mag)
 
-victors_merge$mag <- gsub(pattern = "victors_",
-                                   replacement = "",
-                                   x = victors_merge$mag)
+vf_merge$mag <- gsub(pattern = paste0(vf_prefix, "_"), 
+                     replacement = "", 
+                     x = vf_merge$mag)
 
 #5) Save merge dfs to file 
 
-vfdb_merge <- vfdb_merge %>%
+vf_merge <- vf_merge %>%
   mutate(Sequence_id = paste0(mag, "_", qseqid)) %>%
   select(mag, Sequence_id, sseqid, header_class, evalue, bitscore) %>%
-  rename(Genome = mag, VFDB_VF_found = sseqid, VFDB_VF_class = header_class)
+  rename(Genome = mag, VF_found = sseqid, VF_class = header_class)
 
-victors_merge <- victors_merge %>%
-  mutate(Sequence_id = paste0(mag, "_", qseqid)) %>%
-  select(mag, Sequence_id, sseqid, header_class, evalue, bitscore) %>%
-  rename(Genome = mag, Victor_VF_found = sseqid, Victor_VF_class = header_class)
+# Save the modified dataframe to a file with a dynamic name based on vf_prefix
+data.table::fwrite(vf_merge, file = paste0(out.path, "/", vf_prefix, "_format_gSpread.csv"))
 
+#6) Count the number of Unique Virulence factors in each bin
 
-data.table::fwrite(victors_merge, file=paste0(out.path, "/victors_ann_format.csv"))
-data.table::fwrite(vfdb_merge, file=paste0(out.path, "/vfdb_ann_format.csv"))
-
-#6) Count number of Unique Virulence factors in each bin
-
-vfdb_count <- vfdb_merge %>%
-  select(VFDB_VF_found, Genome, VFDB_VF_class) %>%
-  unique(.) %>%
-  count(Genome) %>%
-  as.data.frame(.)
-
-
-victors_count <- victors_merge %>%
-  select(Victor_VF_found, Genome, Victor_VF_class) %>%
+vf_count <- vf_merge %>%
+  select(VF_found, Genome, VF_class) %>%
   unique(.) %>%
   count(Genome) %>%
   as.data.frame(.)
 
 #6.1) Save count tables to file
-data.table::fwrite(vfdb_count, file=paste0(out.path, "/vfdb_per_genome_unique_count.csv"))
-data.table::fwrite(victors_count, file=paste0(out.path, "/victors_per_genome_unique_count.csv"))
-
-
-
-
-
-
+data.table::fwrite(vf_count, file = paste0(out.path, "/", vf_prefix, "_per_genome_unique_count.csv"))
